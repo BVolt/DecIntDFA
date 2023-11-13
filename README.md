b5d85a38050528c9bad1d7c91176a7a8b43032de
BVolt <Brendenj7@hotmail.com>
2023-11-13 00:47:14 -0800

    md test

---
+++ README.md
@@ -0,0 +1,8 @@
+# Numeric Literals
+Program utilizing deterministic finite automata to recognize numeric literals according to python 3.12 documentation
+
+## Contributors 
+Brenden Johnson - BVOLT
+
+## Tasks
+Brenden Johnson - Decimal Integer NFA/DFA Diagrams and DFA Class
--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-states = ["q1", "q2", "q3", "q4", "q5", "q6"] # Set of States of decimal integer DFA
-acceptstates = ["q2", "q3"] # Set of accept States of decimal integer DFA
-start_state = "q1" # initial state
-alphabet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_"] # alphabet for decimal integer
-transition = { # transition function for all states
-    "q1": lambda c: "q2" if nonzeroDigit(c) else("q3" if c=="0" else "q6"), 
-    "q2": lambda c: "q2" if isDigit(c) else("q4" if c=="_" else "q6"),
-    "q3": lambda c: "q3" if c == "0" else ("q5" if c=="_" else "q6"),
-    "q4": lambda c: "q2" if isDigit(c) else "q6",
-    "q5": lambda c: "q3" if c == "0" else "q6",
-    "q6": lambda c: "q6"
-    }
-
 def main():
     state = start_state # set start state
     str = input(">>") # read input string
@@ -71,3 +58,5 @@ def isDigit(c):
 
 if __name__ == "__main__":
     main()
+
+
